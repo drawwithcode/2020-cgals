cd678b2db9940c4c87141e9b03b30157928f7e2d
alessiaarosio <alessiarosio.aa@gmail.com>
2021-01-20 14:52:48 +0100

    modifiche draw.js e draw.html

--- public/draw.js
+++ public/draw.js
@@ -12,37 +12,79 @@ let isDrawing = false;
 
 let currentPath;
 
-
 let sketch = function(backgroundCanvas) {
   backgroundCanvas.setup = function() {
-    backgroundCanvas.createCanvas(windowWidth, windowHeight);
+    let backgroundC = backgroundCanvas.createCanvas(windowWidth, windowHeight);
     backgroundCanvas.background("#fffbe8");
-    //backgroundCanvas.parent("canvascontainer");
-    //backgroundCanvas.position(0, 0);
 
-    // backgroundCanvas.noStroke();
-    // backgroundCanvas.text("HOME",tx,ty);
-    //
-    // if (backgroundCanvas.overHome(tx, ty) && mouseIsPressed) {
+    backgroundCanvas.push()
+    backgroundCanvas.noStroke();
+    let home = backgroundCanvas.text("HOME",tx,ty);
+    backgroundCanvas.pop()
+
+    // if (overHome(tx, ty) && mouseIsPressed()) {
     // window.open("index.html", "_self")
     // }
+
+    backgroundCanvas.strokeWeight(0.5);
+    backgroundCanvas.rectMode(CENTER);
+
+    let rx = width/2;
+    let ry = height/1.8;
+    let rw = 600;
+    let rh = 45;
+
+    backgroundCanvas.push()
+    if (backgroundCanvas.overRect(rx, ry, rw, rh)) {
+        backgroundCanvas.fill("white");
+        backgroundCanvas.stroke("black");
+      } else {
+        backgroundCanvas.noFill();
+        backgroundCanvas.stroke("black");
+      }
+      backgroundCanvas.rect(windowWidth/2, windowHeight/2 + 150, rw, rh);
+      //backgroundCanvas.mousePressed(saveDrawing)
+    backgroundCanvas.pop()
+
+    backgroundCanvas.push();
+      backgroundCanvas.textAlign(CENTER);
+      backgroundCanvas.text("ADD TO THE CHAIN", windowWidth/2, windowHeight/2 + 155);
+      backgroundCanvas.fill("black");
+      backgroundCanvas.noStroke();
+    backgroundCanvas.pop()
   };
 
-  // backgroundCanvas.draw = function() {
+  backgroundCanvas.draw = function() {
 
-  // };
+    //add border
+    // addWordbutton = backgroundCanvas.rect(rx, ry, rw, rh);
+    // let addToChain = backgroundCanvas.text("ADD TO THE CHAIN",rx,ry+5)
 
-  // backgroundCanvas.overHome = function(x, y) {
-  //   if (mouseX > x && mouseX < + x+50 && mouseY > y/1.5 && mouseY < y) {
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // };
+    // addWordButton.mouseClicked();
+  };
+
+  backgroundCanvas.overRect = function(x, y) {
+    if (backgroundCanvas.mouseX > backgroundCanvas.x && backgroundCanvas.mouseX < + backgroundCanvas.x+50 && backgroundCanvas.mouseY > backgroundCanvas.y/1.5 && backgroundCanvas.mouseY < backgroundCanvas.y) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  backgroundCanvas.mouseClicked = function() {
+    //backgroundCanvas.saveCanvas(canvas, "myCanvas", ".png");
+    window.open("index.html", "_self");
+  };
+
+
+    backgroundCanvas.windowResized = function() {
+      backgroundCanvas.resizeCanvas(windowWidth, windowHeight);
+    };
 
 }
 
 let backgroundCanvas = new p5(sketch);
+let home = new p5(text1);
 
 
 function preload(){
@@ -51,21 +93,27 @@ function preload(){
 
 function setup() {
 // canvas = createCanvas(windowWidth,windowHeight);
-canvas = createCanvas(500, 500);
+canvas = createCanvas(500, 300);
 canvas.background("white");
-// canvas.stroke("black");
-// canvas.strokeWeight(1);
 canvas.mousePressed(startPath);
-canvas.parent("canvascontainer");
+//canvas.parent("canvascontainer");
 canvas.mouseReleased(endPath);
+canvas.position(windowWidth/2 - 250, windowHeight/2 - 250);
+
+push()
+let r = rect(0, 0, 500, 300);
+r.noFill();
+r.stroke("black");
+r.strokeWeight(1);
+pop()
 
 database = firebase.database();
 
 let ref = database.ref("drawings");
 ref.on("value", gotData, errData);
 
-let saveButton = select("#saveButton");
-saveButton.mousePressed(saveDrawing);
+// let saveButton = select("#saveButton");
+// saveButton.mousePressed(saveDrawing);
 
 }
 
@@ -79,25 +127,17 @@ function endPath() {
   isDrawing = false;
 }
 
-function overHome(x, y) {
-	if (mouseX > x && mouseX < + x+50 && mouseY > y/1.5 && mouseY < y) {
-	  return true;
-	} else {
-	  return false;
-	}
-}
-
 function draw() {
 // background("#fffbe8");
 
-push()
-noStroke();
-let home = text("HOME",tx,ty);
-pop()
+// push()
+// noStroke();
+// let home = text("HOME",tx,ty);
+// pop()
 
-if (overHome(tx, ty) && mouseIsPressed) {
-window.open("index.html", "_self")
-}
+// if (overHome(tx, ty) && mouseIsPressed) {
+// window.open("index.html", "_self")
+// }
 
 
 if (isDrawing) {
@@ -119,20 +159,13 @@ for (var j = 0; j < path.length; j++) {
 }
 endShape();
 }
+
+
 // strokeWeight(0.5);
 // rectMode(CENTER);
 //
 // let rx = width/2;
 // let ry = height/1.8;
-//
-//
-//
-// let textbox;
-// let inputText = "Enter your word here";
-// let addWordButton;
-// let buttonText = "ADD TO THE CHAIN";
-//
-//
 //
 // push()
 //
@@ -149,41 +182,24 @@ endShape();
 //   rect(rx, height/2.5, rw, rh)
 //   pop()
 //
-//
-// push()
-// textbox = createInput(inputText);
-// textbox.position(rx/2.33, height/2.75);
-// textbox.size(568, rh-3);
-// textbox.style("font-size", 10 + "pt").style("padding-left", 20 + "pt").style("boxShadow", "none").style("border", 0 + "px");
-// pop()
-//
-//
 // //add border
 // addWordbutton = rect(rx, ry, rw, rh);
 // textAlign(CENTER);
 // let addToChain = text("ADD TO THE CHAIN",rx,ry+5)
-//
-// // myText = textbox.value();
-//
-//
-// textAlign(CENTER)
-// let home = text("HOME",tx,ty);
-//
-// if (overHome(tx, ty) && mouseIsPressed) {
-// window.open("index.html", "_self")
-// }
-//
+
+// myText = textbox.value();
+
 // addWordButton.mouseClicked();
 }
 
-function saveDrawing() {
-  var ref = database.ref("drawings");
-  var data = {
-    drawing: drawing
-  }
-  ref.push(data);
-  saveCanvas(canvas, "myCanvas", "jpg");
-}
+// function saveDrawing() {
+//   // var ref = database.ref("drawings");
+//   // var data = {
+//   //   drawing: drawing
+//   // }
+//   // ref.push(data);
+//   saveCanvas(canvas, "myCanvas");
+// }
 
 function errData(err) {
   console.log("Error");
@@ -197,10 +213,8 @@ let keys = Object.keys(drawings);
 console.log(keys);
 for (var i = 0; i < keys.lenght; i++) {
   let k = keys[i];
-
+  }
 }
-}
-
 
 function overRect(x, y, w, h) {
 	if (mouseX > x - w/2 && mouseX < x+w/2 && mouseY > y - h/2 && mouseY < y+h/2) {
@@ -210,13 +224,13 @@ function overRect(x, y, w, h) {
 	}
 }
 
-function overHome(x, y) {
-	if (mouseX > x && mouseX < + x+50 && mouseY > y/1.5 && mouseY < y) {
-	  return true;
-	} else {
-	  return false;
-	}
-}
+// function overHome(x, y) {
+// 	if (mouseX > x && mouseX < + x+50 && mouseY > y/1.5 && mouseY < y) {
+// 	  return true;
+// 	} else {
+// 	  return false;
+// 	}
+// }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
