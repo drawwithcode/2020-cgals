d03f4b812e3ec4a0fed189fc5fed72a4c9182aae
alessiarosio-aa <alessia.arosio@mail.polimi.it>
2021-01-24 13:25:58 +0100

    aggiunta rettangolo nel draw

--- public/draw.js
+++ public/draw.js
@@ -165,6 +165,13 @@ endShape();
 // addWordButton.mouseClicked();
 }
 
+// function rectangle() {
+//   let r = select("#rettangolo");
+//   r.style("width", 600 + "px").style("height", 380 + "px").style("border", 0.5 + "px", "solid", "#000000");    //.style("border", 2 + "px")
+//   r.center();
+//   r.position(windowWidth/2, windowHeight/2);
+// }
+
 function saveDrawing() {
   var ref = database.ref("words");
     loadPixels();
@@ -197,16 +204,6 @@ for (var i = 0; i < keys.lenght; i++) {
   let k = keys[i];
 }
 }
-
-let bordo = function(b) {
-  b.setup = function() {
-    b.rect(b.windowWidth/2 - 300, b.windowHeight/2 + 190, 600, 45);
-    b.strokeWeight(1);
-    b.stroke("black");
-    b.noFill();
-  }
-}
-let b = new p5(bordo);
 
 // function overRect(x, y, w, h) {
 // 	if (mouseX > x - w/2 && mouseX < x+w/2 && mouseY > y - h/2 && mouseY < y+h/2) {
